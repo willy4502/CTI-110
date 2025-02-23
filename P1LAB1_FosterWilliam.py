68b6f3e13004140c5f79a68dcf191e99dedb9345
willy4502 <fosterw4502@student.faytechcc.edu>
2025-02-22 20:46:34 -0500

    Delete P1LAB1_FosterWilliam.py

--- P1LAB1_FosterWilliam.py
+++
@@ -1,8 +0,0 @@
- # William Foster
- # 2-22-2025
- # P1LAB1
- # Making code that lets the user input their first and last name
-
-first_name = (input('Enter your first name: '))
-last_name = (input('Enter your last name: '))
-print ('Hello, ',first_name, last_name,'! Welcome to CTI-110')
